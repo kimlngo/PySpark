c4b9f4b62bb3c6b34fc71ef7c6a2925c0f37c3b4
Kim Long Ngo <ngokimlong2007@gmail.com>
2026-01-11 16:26:30 -0500

    First RDD with Movie Dataset

---
+++ 01-Getting Started/ratings-counter.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+#execution command: spark-submit ratings-counter.py
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("file:///github/PySpark/01-Getting Started/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print(f"{key} {value}")
