c6b7607ef2a1411f182adcf1fa414bdbae63bfb4
Kim Long Ngo <ngokimlong2007@gmail.com>
2026-01-13 14:54:09 -0500

    Process Word Count using flatMap

---
+++ 01-Getting Started/word-count.py
@@ -0,0 +1,30 @@
+import re
+from pyspark import SparkContext, SparkConf
+
+def normalizeWord(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+def matchWordCount(word):
+    return (word, 1)
+
+def reduceBySum(v1, v2):
+    return v1 + v2
+
+def flipTuple(tup):
+    return (tup[1], tup[0])
+
+conf = SparkConf().setMaster("local").setAppName("WordCountApp")
+sc = SparkContext(conf = conf)
+
+wordCount = (sc.textFile("file:///github/PySpark/01-Getting Started/book.txt")
+           .flatMap(normalizeWord)
+           .map(matchWordCount)
+           .reduceByKey(reduceBySum)
+           .map(flipTuple)
+           .sortByKey()
+           .collect())
+
+for count, word in wordCount:
+    cleanWord = word.encode('ascii', 'ignore')
+    if cleanWord:
+        print(f"{cleanWord} \t\t {count}")
